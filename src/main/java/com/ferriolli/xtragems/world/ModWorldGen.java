f6c576b1755608f5012e61069a769ca9c79afb98
Pedro <p_pauloferriolli@hotmail.com>
2020-11-02 18:57:15 -0300

    geracao de minerios no nether

--- src/main/java/com/ferriolli/xtragems/world/ModWorldGen.java
+++ src/main/java/com/ferriolli/xtragems/world/ModWorldGen.java
@@ -2,6 +2,8 @@ package com.ferriolli.xtragems.world;
 
 import com.ferriolli.xtragems.Init.ModBlocks;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -17,10 +19,17 @@ public class ModWorldGen implements IWorldGenerator {
         if(world.provider.getDimension() == 0){
             generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
         }
+        if (world.provider.getDimension() == -1){
+            generateNether(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+        }
     }
 
     private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider){
-        generateOre(ModBlocks.RUBY_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, random.nextInt(6) + 1, 30);
+        generateOre(ModBlocks.RUBY_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 8, 16, random.nextInt(6) + 1, 12);
+    }
+
+    private void generateNether(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider){
+        generateOreNether(ModBlocks.BLACK_GEM_ORE.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 0, 100, random.nextInt(5) + 1, 20);
     }
 
     private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances){
@@ -29,6 +38,16 @@ public class ModWorldGen implements IWorldGenerator {
         for (int i = 0; i < chances; i++){
             BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
             WorldGenMinable generator = new WorldGenMinable(ore, size);
+            generator.generate(world, random, pos);
+        }
+    }
+
+    private void generateOreNether(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances){
+        int deltaY = maxY - minY;
+
+        for (int i = 0; i < chances; i++){
+            BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
+            WorldGenMinable generator = new WorldGenMinable(ore, size, BlockMatcher.forBlock(Blocks.NETHERRACK));
             generator.generate(world, random, pos);
         }
     }
